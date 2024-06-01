caad996601222873284b20df25d28c70732b395c
SHIV SINGH <shivrajawat013489@outlook.com>
2024-06-01 21:53:24 +0530

    Commit on dated 010624

---
+++ src/CollectionFrmwrkDemo/CursorDemo.java
@@ -0,0 +1,12 @@
+package CollectionFrmwrkDemo;
+
+import java.util.LinkedList;
+
+public class CursorDemo {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ src/SampleCodesDemo/ReverseStringValue.java
@@ -0,0 +1,57 @@
+package SampleCodesDemo;
+
+public class ReverseStringValue {
+	
+
+	public String GetReversalString(String str)
+	{
+		String reverse = "";
+		
+		for(int i=0;i<str.length();i++)
+		{
+			reverse = str.charAt(i)+reverse;			
+		}	
+		
+		return reverse;		
+		
+	}
+	
+	//A string is a palindrome when it stays the same on reversing the order of characters in that string.
+	//It can be achieved by reversing the original string first and then checking if the reversed 
+	//string is equal to the original string.
+	
+	public void CheckStringPalindrome(String str)
+	{
+		String reverse = GetReversalString(str);
+		
+		if(reverse.equals(str))
+		{
+		 System.out.println("Given String is Palindrome");	
+		}	
+		else
+		{
+		 System.out.println("String not a palindrome");	
+		}
+		
+	}
+	
+
+	
+	
+	public static void main(String[] args) 
+	{
+		ReverseStringValue r = new ReverseStringValue();
+		
+		System.out.println(r.GetReversalString("Rajawat"));
+		
+	    r.CheckStringPalindrome("noon");
+	    
+	    
+	    
+
+		
+		
+
+	}
+
+}
